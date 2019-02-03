bf09bf071595c6f7306b3f754f4318405e18ca80
Sigalou <sigalou@sigalou-domotique.fr>
2019-02-03 20:24:29 +0000

    Creation manuelle du Cookie Amazon

--- resources/nodejs.sh
+++ resources/nodejs.sh
@@ -4,8 +4,8 @@ touch /tmp/${2}_dep
 echo "Début de l'installation"
 
 echo 0 > /tmp/${2}_dep
-echo "Suppression du dossier : alexaapi/ressources/alexa-remote-http"
-rm alexa-remote-http -r
+#echo "Suppression du dossier : alexaapi/ressources/alexa-remote-http"
+#rm alexa-remote-http -r
 echo "Récupération de la derniere version de alexa-remote-http"
 git clone https://github.com/sx-1/alexa-remote-http.git
 cd alexa-remote-http
