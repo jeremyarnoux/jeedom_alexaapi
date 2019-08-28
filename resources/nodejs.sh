3ece701e8c115cd266d4dc0d1992e549758e11eb
Nebz <28622481+NebzHB@users.noreply.github.com>
2019-08-28 11:34:27 +0200

    Update nodejs.sh

--- resources/nodejs.sh
+++ resources/nodejs.sh
@@ -73,7 +73,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y lsb_release
 
 lsb_release -c | grep jessie
-if [ $? -eq 0]
+if [ $? -eq 0 ]
 then
   installVer='8' 	#NodeJS major version to be installed
   minVer='8'	#min NodeJS major version to be accepted  
