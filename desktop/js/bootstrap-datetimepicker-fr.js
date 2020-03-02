710d53e32290eaf6a222cf7b3b80ab736ae1314b
Aidom <57922677+Aidom@users.noreply.github.com>
2020-03-02 18:17:34 +0100

    Added datetimepicker to scenario template
    Simplify the use of the date time input

---
+++ desktop/js/bootstrap-datetimepicker-fr.js
@@ -0,0 +1,13 @@
+$.fn.datetimepicker.dates['fr'] = {
+days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+daysMin: ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+monthsShort: ["Janv", "Févr", "Mars", "Avril", "Mai", "Juin", "Juil", "Août", "Sept", "Oct", "Nov", "Déc"],
+today: "Aujourd'hui",
+monthsTitle: "Mois",
+clear: "Effacer",
+weekStart: 1,
+meridiem: '',
+};
+
