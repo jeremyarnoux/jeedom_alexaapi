86214ef9cac7a51b97c7493c87d8bc32d4469410
skillix <68369861+skillix@users.noreply.github.com>
2021-10-13 11:18:29 +0200

    faire parler Alexa avec plus de 250 caractères
    Découpe les grands textes en les découpant avec la ponctuation. Au niveau des points, puis si encore trop grand au niveau des virgules, et a défaut fera un découpage en fonction du nombre de mots.

--- resources/alexaapi.js
+++ resources/alexaapi.js
@@ -211,9 +211,65 @@ CommandAlexa.Speak = function(req,res){
 	}
 
 	let Commands = [];
-	var test = ('volume' in req.query === true) && (req.query.volume != "");
-	if (test) 											Commands.push({command: 'volume', value: req.query.volume});
-														Commands.push({command: SpeakouAnnouncement, value: req.query.text});
+	var test = ('volume' in req.query === true) && (req.query.volume != ""); 
+  	
+	if (test) 									Commands.push({command: 'volume', value: req.query.volume});
+  
+    var TXTchaine = req.query.text; 
+	var Nbparties;
+  	var Decoupe;
+  	var TABdecoupe;
+    var NBmots;
+  	var TABdecoupePoint;
+  	var TABdecoupeVirgule;
+  
+if (TXTchaine.length < 250)	{				
+    Commands.push({command: SpeakouAnnouncement, value: req.query.text});
+}
+else {										
+        TABdecoupePoint = req.query.text.split(". "); // on met chaque phrase separer d'un point dans dans des element d'un tableau
+        for(var i = 0; i < TABdecoupePoint.length; i++) { //on decoupe le texte au niveau des point
+            Decoupe = TABdecoupePoint[i];
+            if (Decoupe.length < 250) {                //si c'est assez petit on dit les morceaux
+                Commands.push({command: SpeakouAnnouncement, value: Decoupe});
+            }
+            else {
+                TABdecoupeVirgule = Decoupe.split(", ");     // sinon si c'est encore trop long on decoupe au virgules
+                for(var ii = 0; ii < TABdecoupeVirgule.length; ii++) { 
+                    Decoupe = TABdecoupeVirgule[ii];
+                    if (Decoupe.length < 250) {              // si c'est assez court on dit les morceaux
+                        Commands.push({command: SpeakouAnnouncement, value: Decoupe});
+                    }
+                    else{																// sinon decoupage au nombre de mot
+                        Nbparties = parseInt(Decoupe.length / 220) +1;  // on determine en combien de morceaux on prend 30 carac de marge car le nombre de caractere differe d'un mot a l'autre
+                        TABdecoupe = Decoupe.split(" "); // on met chaque mot dans dans des element d'un tableau
+						NBmots = TABdecoupe.length; //on determine le nombre de mots dans la phrase
+                                                                                
+                        var TXTdebut = 0;   //debut 1er mot de la 1ere phrase
+                        var TXTsuiteInit = parseInt(NBmots / Nbparties)-1; //ont determine le dernier mot de la 1ere phrase
+                        var TXTsuite = TXTsuiteInit; //on sauvegarde le nombre de mot par morceau
+                        var TXTfin = NBmots-1; // on determine le numero du dernier mot de la derniere phrase
+                        var Phrase = '';
+      
+                        for(var x = Nbparties; x > -1; x--) {                   // autant de boucle que de morceau
+                            for(var xx = TXTdebut; xx <= TXTsuite; xx ++){         //autant de boucle que de mot par morceau
+                                 Phrase = Phrase + " " + TABdecoupe[xx];	//on construit le premier morceau mot par mot
+                            }
+                            TXTdebut = TXTsuite + 1;				//le mot suivant le dernier mot du morceau precedent devient le 1er mot du morceau suivant
+                            if (x > 1) TXTsuite = TXTsuite + TXTsuiteInit;          //on dermine le dernier mot de la boucle suivant (nombr ed emot deja prononcé + le nombre de mot par morceau) sauf si dernier morceau
+                            else TXTsuite = TXTfin;                                     // dernier morceau le dernier mot est le dernier mot de la phrase initial                 
+                            
+         
+                          	Commands.push({command: SpeakouAnnouncement, value: Phrase}); // je prononce la phase de mon morceau
+                        	Phrase = "";
+                       } 
+                    }
+                }
+            }
+        }
+
+}
+  
 	if (('lastvolume' in req.query === true) && test) 	Commands.push({command: 'volume', value: req.query.lastvolume});
 	
 	boucleSurSerials_sendMultiSequenceCommand(req, Commands);
