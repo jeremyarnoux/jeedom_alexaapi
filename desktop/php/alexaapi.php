5209396683b12484334df505325202543164e41c
JEALG <jalain.gosse@icloud.com>
2022-04-02 11:37:13 +0200

    correction resetSearch

--- desktop/php/alexaapi.php
+++ desktop/php/alexaapi.php
@@ -117,7 +117,7 @@ foreach ($eqLogics as $eqLogic) {
             <div class="input-group" style="margin-bottom:5px;">
                 <input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
                 <div class="input-group-btn">
-                    <a id="bt_resetObjectSearch" class="btn" style="width:30px"><i class="fas fa-times"></i>
+                    <a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i>
                     </a><a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
                 </div>
             </div>
